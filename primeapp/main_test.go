8e95b2d6470c885ea19877e8674eb69dead02796
Kyler Jacobson <40446054+KylerJacobson@users.noreply.github.com>
2025-01-24 16:32:53 -0700

    add first table test

--- primeapp/main_test.go
+++ primeapp/main_test.go
@@ -3,12 +3,34 @@ package main
 import "testing"
 
 func Test_isPrime(t *testing.T) {
-	result, msg := isPrime(0)
-	if result {
-		t.Errorf("with %d as test parameter, got true, but expected false", 0)
+	primeTests := []struct {
+		name     string
+		testNum  int
+		expected bool
+		msg      string
+	}{
+		{
+			"Prime", 7, true, "7 is a prime number",
+		},
+		{
+			"Negative", -1, false, "Negative numbers are not prime, by definition",
+		},
+		{
+			"Not Prime", 77, false, "77 is not prime, it is divisible by 7",
+		},
+		{
+			"Zero", 0, false, "0 is not prime, by definition",
+		},
 	}
 
-	if msg != "0 is not prime, by definition" {
-		t.Error("wrong messages returned: ", msg)
+	for _, e := range primeTests {
+		result, msg := isPrime(e.testNum)
+		if e.expected != result {
+			t.Errorf("%s expected %t but got %t for %d", e.name, e.expected, result, e.testNum)
+		}
+
+		if msg != e.msg {
+			t.Errorf("%s expected message '%s' but got '%s'", e.name, e.msg, msg)
+		}
 	}
 }
